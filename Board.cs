3ad276701d374f71a88327371ce8c7efcc96435b
Dmitrijs Balcers <dmitrijs.balcers@gmail.com>
2012-09-02 20:57:07 +0300

    commit develop2

--- Board.cs
+++ Board.cs
@@ -47,7 +47,7 @@ namespace Halma_v0._3
             int marginY;
             int incValue;
 
-            // Calculates the increments so that we can get a squared board develop
+            // Calculates the increments so that we can get a squared board develop develop
  
             if (d.Width < d.Height)
             {
